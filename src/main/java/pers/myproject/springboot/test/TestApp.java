be32f160535416a9c65c7ba96158b7ce2893d374
liushiyang <liushiyang@163.com>
2019-05-02 15:00:29 +0800

    33

--- src/main/java/pers/myproject/springboot/test/TestApp.java
+++ src/main/java/pers/myproject/springboot/test/TestApp.java
@@ -3,6 +3,6 @@ package pers.myproject.springboot.test;
 public class TestApp {
 
     public static void main(String[] args) {
-        System.out.println("dev-02");
+        System.out.println("dev-02  !!!!");
     }
 }
